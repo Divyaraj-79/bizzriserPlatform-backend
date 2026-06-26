55a8eab6011b3494c9dffb060d6d9ebbb0f31154
Divyaraj-79 <divyarajmakwana79@gmail.com>
2026-06-26 17:25:57 +0530

    chore: untrack dist folder and ignore build output

--- dist/common/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './filters/global-exception.filter';
-export * from './interceptors/transform.interceptor';
-export * from './interceptors/logging.interceptor';
-export * from './services/security.service';
--- dist/modules/analytics/dto/analytics-query.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class AnalyticsQueryDto {
-    accountId?: string;
-    startDate?: string;
-    endDate?: string;
-}
