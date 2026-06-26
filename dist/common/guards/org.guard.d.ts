6e1b0d949a61555098ae8448b94b692a54d55499
Divyaraj-79 <divyarajmakwana79@gmail.com>
2026-06-26 17:02:08 +0530

    remove dist folder from git tracking

--- dist/common/guards/org.guard.d.ts
+++
@@ -1,4 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-export declare class OrganizationGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean;
-}
--- dist/modules/roles/dto/create-role.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateRoleDto {
-    name: string;
-    description?: string;
-    color?: string;
-    permissions?: string[];
-    isDefault?: boolean;
-}
